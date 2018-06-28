02b59e7be2421b42623c1b0e28c60d582ca2795e
Nate Olson <nolson@nist.gov>
2018-06-28 06:36:47 -0400

    speed up munging

--- munge/02_zvi_mrexp.R
+++ munge/02_zvi_mrexp.R
@@ -1,4 +1,4 @@
 ## Create MRexperiment from biom
 ## Use biomformat package and metagenomeSeq
 mrexp <- phyloseq_to_metagenomeSeq(ps)
-ProjectTemplate::cache("mrexp")
+ProjectTemplate::cache("mrexp", depends = c("ps"))
